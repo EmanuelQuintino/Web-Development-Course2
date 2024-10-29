a3e379e141c3dfa49a2a59504402d8a6bc036e01
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-10-29 20:31:27 -0300

    add array manipulation examples

--- 2-JavaScript/class-04-data-manipulation/script.js
+++ 2-JavaScript/class-04-data-manipulation/script.js
@@ -14,7 +14,7 @@ console.log(myName.slice(-3));
 console.log(myName.slice(2, -3));
 
 console.log(concept.length);
-console.log(concept.indexOf("o", 5));
+console.log(concept.indexOf("web", 5));
 
 // numbers
 
@@ -36,9 +36,39 @@ console.log(Math.ceil(5.001));
 console.log(Math.floor(5.999));
 
 console.log(Math.random());
-console.log(Math.ceil(Math.random() * 10));
+console.log(Math.ceil(Math.random() * 10)); //⭐
 
 const times = ["Corinthians", "Flamengo", "Vasco", "Fortaleza", "Ceará", "São Paulo"];
 
 const randomNumber = Math.floor(Math.random() * times.length);
 console.log(times[randomNumber]);
+
+// array
+const crush = ["Tiazinha", "Rambo", "Bolzo", "Paola Oliveira", "Mariana Rui Barbosa"];
+
+console.log(crush.length);
+console.log(crush[1]);
+crush[0] = "Feiticeira";
+console.log(crush);
+
+let a = "Emanuel";
+a[1] = "M"; // string is immutable!
+console.log(a);
+
+console.log(crush.join("-"));
+console.log("Bem vindo à aula".replaceAll(" ", "-"));
+
+console.log(crush.includes("Rambo"));
+console.log("João Emanuel Vieira Quintino".split(" ")[1]);
+console.log(crush.reverse());
+console.log("Emanuel".split("").reverse().join(""));
+
+// Crie uma função que receba um texto como parâmetro e diga quantas palavras ele tem.
+
+function wordCount(text) {
+  const count = text.split(" ").length;
+  return count;
+}
+const text = "Emanuel Quintino";
+
+console.log(wordCount(text));
