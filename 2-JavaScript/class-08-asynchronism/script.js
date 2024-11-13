b54c929e9a2cc83500c57f3643cb507eb8efff33
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-11-13 18:29:17 -0300

    add class 09 dom

--- 2-JavaScript/class-08-asynchronism/script.js
+++ 2-JavaScript/class-08-asynchronism/script.js
@@ -1,32 +1,32 @@
-// // timeout and interval
+// timeout and interval
 
-// console.log("Antes");
+console.log("Antes");
 
-// setTimeout(() => {
-//   console.log("Timeout");
-// }, 0);
+setTimeout(() => {
+  console.log("Timeout");
+}, 0);
 
-// setInterval(() => {
-//   console.log("Interval");
-// }, 0);
+setInterval(() => {
+  console.log("Interval");
+}, 0);
 
-// setInterval(() => {
-//   const clock = new Date();
-//   // console.log(clock.toLocaleTimeString());
-// }, 10);
+setInterval(() => {
+  const clock = new Date();
+  // console.log(clock.toLocaleTimeString());
+}, 10);
 
-// console.log("Depois");
+console.log("Depois");
 
-// // promises
+// promises
 
-// const urlAPI = "https://jsonplaceholder.typicode.com";
+const urlAPI = "https://jsonplaceholder.typicode.com";
 
-// // "https://jsonplaceholder.typicode.com/users"
-// fetch(urlAPI + "/users")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+// "https://jsonplaceholder.typicode.com/users"
+fetch(urlAPI + "/users")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
-// console.log("Final");
+console.log("Final");
 
 for (let i = 0; i < 200000; i++) {
   console.log(i);
