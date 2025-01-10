cfbf14b41f634ac79ac98dd3613187edc71a066b
Emanuel Quintino <emanuelquintino@hotmail.com>
2025-01-10 19:02:45 -0300

    add class 03 hooks

---
+++ 3-React/class-03-hooks/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState(["user1", "user2", "user3"]);
+
+  console.log(count);
+
+  function add() {
+    setCount(count + 1);
+  }
+
+  function handleName(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
+
+  return (
+    <>
+      <h1>Class 03 Hooks</h1>
+
+      <p>Count: {count}</p>
+      <button onClick={add}>add</button>
+
+      <p>Name: {name}</p>
+      <input type="text" onInput={handleName} />
+
+      {users.map((user) => {
+        return <p key={user}>{user}</p>;
+      })}
+    </>
+  );
+}
