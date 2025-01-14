5aca1aa421f2f356c248ebf3be0dbd74fa9c1465
Emanuel Quintino <emanuelquintino@hotmail.com>
2025-01-14 20:55:39 -0300

    add class 04 context

---
+++ 3-React/class-04-context/src/contexts/ValueContext.tsx
@@ -0,0 +1,9 @@
+import { createContext, PropsWithChildren } from "react";
+
+export const ValueContext = createContext({});
+
+export function ValueProvider({ children }: PropsWithChildren) {
+  return <ValueContext.Provider value={0}>
+    {children}
+  </ValueContext.Provider>;
+}
