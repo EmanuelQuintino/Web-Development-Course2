aae6f7928f9f37bdb70dc3e6b63b8845453f1bfe
Emanuel Quintino <emanuelquintino@hotmail.com>
2025-02-19 19:16:27 -0300

    add class node

--- 4-Node.js/class-02-routes/src/routes/index.ts
+++ 4-Node.js/class-02-routes/src/routes/index.ts
@@ -5,3 +5,11 @@ export const router = Router();
 router.get("/", (_request, response) => {
   response.json({ name: "Class Routes" });
 });
+
+router.get("/user", (_req, res) => {
+  res.json([
+    { name: "Ash", age: 10 },
+    { name: "Mestre Came", age: 300 },
+    { name: "Alucard", age: 500 },
+  ]);
+});
