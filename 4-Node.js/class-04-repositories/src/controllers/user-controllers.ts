4de85a08876a30d00a15cd960bb7830be6a1d8e9
Emanuel Quintino <emanuelquintino@hotmail.com>
2025-02-25 20:50:36 -0300

    add class 04 repositories

---
+++ 4-Node.js/class-04-repositories/src/controllers/user-controllers.ts
@@ -0,0 +1,56 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/app-error";
+import { z } from "zod";
+import { sqliteConnection } from "../databases";
+
+// zod
+const UserSchema = z.object({
+  name: z.string({ message: "Nome é obrigatório!" }).min(3, "Mínimo de 3 caracteres!"),
+  email: z.string().email(),
+  password: z.string().min(3),
+});
+
+export const userControllers = {
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { name, email, password } = UserSchema.parse(req.body);
+
+      const user = {
+        id: "2",
+        name,
+        email,
+        password,
+      };
+
+      const db = await sqliteConnection();
+
+      const sqlQuery =
+        "INSERT INTO users (id, name, email, password) VALUES (?, ?, ?, ?)";
+
+      await db.run(sqlQuery, [user.id, user.name, user.email, user.password]);
+
+      res.status(201).json({ message: "user created" });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  read(req: Request, res: Response) {
+    const { id } = req.params;
+    res.status(200).json({ message: `user ${id} read` });
+  },
+
+  update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, series } = req.body;
+
+    console.log({ id, name, series });
+
+    res.status(200).json({ message: "user updated" });
+  },
+
+  delete(req: Request, res: Response) {
+    const { id } = req.params;
+    res.status(200).json({ message: `user ${id} deleted` });
+  },
+};
---
+++ 4-Node.js/class-04-repositories/src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { router } from "./routes";
+import { appErrors } from "./errors/app-errors";
+import { sqliteConnection } from "./databases";
+import { runMigrations } from "./databases/migrations";
+
+const app = express();
+
+const PORT = process.env.PORT || 3333;
+
+// middleware
+app.use(express.json());
+app.use(router); // next
+app.use(appErrors);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}...`);
+});
+
+sqliteConnection()
+  .then(() => console.log("Database is connected..."))
+  .catch((error) => console.error(error));
+
+runMigrations();
